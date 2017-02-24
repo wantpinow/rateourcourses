6b99582baf9516ff2deedf410271f4d4a7692446
Patrick Frenett <pfrenett19@amherst.edu>
2017-02-24 10:39:25 -0500

    Update mysqli_connect.php

--- site/mysqli_connect.php
+++ site/mysqli_connect.php
@@ -1,26 +1,10 @@
 <?php
-// Opens a connection to the database
-// Since it is a php file it won't open in a browser 
-// It should be saved outside of the main web documents folder
-// and imported when needed
-
-/*
-Command that gives the database user the least amount of power
-as is needed.
-GRANT INSERT, SELECT, DELETE, UPDATE ON test3.* 
-TO 'studentweb'@'localhost' 
-IDENTIFIED BY 'turtledove';
-SELECT : Select rows in tables
-INSERT : Insert new rows into tables
-UPDATE : Change data in rows
-DELETE : Delete existing rows (Remove privilege if not required)
-*/
 
 // Defined as constants so that they can't be changed
-DEFINE ('DB_USER', 'rateourc_webcon');
-DEFINE ('DB_PASSWORD', '634qswil');
+DEFINE ('DB_USER', '');
+DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'rateourcourses.com');
-DEFINE ('DB_NAME', 'rateourc_main');
+DEFINE ('DB_NAME', '');
 
 // $dbc will contain a resource link to the database
 // @ keeps the error from showing in the browser
